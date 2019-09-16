12e0a5d1d7e6219e3c642baf0bca64b943121897
ejp4604 <ejp4604@g.rit.edu>
2019-09-16 16:52:58 -0400

    Base Level Production Helper Class

---
+++ Project Arena/Assets/Scripts/MyGameManager_Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectArena
+{
+    public class MyGameManager_Test : MonoBehaviour
+    {
+        public List<GameObject> environmentPieces;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            BuildLevel();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void BuildLevel()
+        {
+            Instantiate(environmentPieces[0], new Vector3(0, 0, 15), Quaternion.identity);
+            Instantiate(environmentPieces[0], new Vector3(0, 15, 0), new Quaternion(0, Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0));
+            Instantiate(environmentPieces[0], new Vector3(0, 0, -15), new Quaternion(Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0, 0));
+            Instantiate(environmentPieces[0], new Vector3(0, -15, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, Mathf.Sqrt(.5f)));
+            Instantiate(environmentPieces[0], new Vector3(15, 0, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, 0));
+            //Instantiate(environmentPieces[0], new Vector3(0, -15, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, Mathf.Sqrt(.5f)));
+        }
+    }
+}
---
+++ Project Arena/Assets/Scripts/Utilities_Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectArena
+{
+    public class Utilities_Test : MonoBehaviour
+    {
+        MyGameManager_Test myGameManager;
+        private void Start()
+        {
+            myGameManager = GameObject.Find("MyGameManager").GetComponent<MyGameManager_Test>();
+        }
+
+
+    }
+}
