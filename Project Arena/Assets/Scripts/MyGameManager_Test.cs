6d50f217256e3ec150a5e01052f19c41c959a3cd
ejp4604 <ejp4604@g.rit.edu>
2019-09-23 16:56:43 -0400

    Finished wall construction and added randomly produced cubic platforms

--- Project Arena/Assets/Scripts/MyGameManager_Test.cs
+++ Project Arena/Assets/Scripts/MyGameManager_Test.cs
@@ -7,6 +7,9 @@ namespace ProjectArena
     public class MyGameManager_Test : MonoBehaviour
     {
         public List<GameObject> environmentPieces;
+        public GameObject arena;
+
+        const float CUBE_DIMENSION = 15f;
         
         // Start is called before the first frame update
         void Start()
@@ -22,12 +25,33 @@ namespace ProjectArena
 
         void BuildLevel()
         {
-            Instantiate(environmentPieces[0], new Vector3(0, 0, 15), Quaternion.identity);
-            Instantiate(environmentPieces[0], new Vector3(0, 15, 0), new Quaternion(0, Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0));
-            Instantiate(environmentPieces[0], new Vector3(0, 0, -15), new Quaternion(Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0, 0));
-            Instantiate(environmentPieces[0], new Vector3(0, -15, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, Mathf.Sqrt(.5f)));
-            Instantiate(environmentPieces[0], new Vector3(15, 0, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, 0));
-            //Instantiate(environmentPieces[0], new Vector3(0, -15, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, Mathf.Sqrt(.5f)));
+            GameObject wall;
+
+            wall = Instantiate(environmentPieces[0], new Vector3(0, 0, 15), Quaternion.identity);
+            wall.transform.SetParent(arena.transform);
+            wall = Instantiate(environmentPieces[0], new Vector3(0, 15, 0), new Quaternion(0, Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0));
+            wall.transform.SetParent(arena.transform);
+            wall = Instantiate(environmentPieces[0], new Vector3(0, 0, -15), new Quaternion(Mathf.Sqrt(.5f), Mathf.Sqrt(.5f), 0, 0));
+            wall.transform.SetParent(arena.transform);
+            wall = Instantiate(environmentPieces[0], new Vector3(0, -15, 0), new Quaternion(Mathf.Sqrt(.5f), 0, 0, Mathf.Sqrt(.5f)));
+            wall.transform.SetParent(arena.transform);
+            wall = Instantiate(environmentPieces[0], new Vector3(15, 0, 0), new Quaternion(Mathf.Sqrt(.5f), 0, Mathf.Sqrt(.5f), 0));
+            wall.transform.SetParent(arena.transform);
+            wall = Instantiate(environmentPieces[0], new Vector3(-15, 0, 0), new Quaternion(0, -Mathf.Sqrt(.5f), 0, Mathf.Sqrt(.5f)));
+            wall.transform.SetParent(arena.transform);
+
+            PlatformConstruction();
+        }
+
+        void PlatformConstruction()
+        {
+            GameObject platform;
+            for(int x = 0; x < 15; x++)
+            {
+                platform = Instantiate(environmentPieces[1], new Vector3(Random.Range(-CUBE_DIMENSION, CUBE_DIMENSION), Random.Range(-CUBE_DIMENSION, CUBE_DIMENSION), Random.Range(-CUBE_DIMENSION, CUBE_DIMENSION)), Quaternion.identity);
+                platform.GetComponent<Transform>().localScale *= Random.Range(1f, CUBE_DIMENSION / 2f);
+                platform.transform.SetParent(arena.transform);
+            }
         }
     }
 }
