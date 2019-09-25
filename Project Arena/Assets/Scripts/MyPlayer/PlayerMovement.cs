96f877ea43cca9805d0cdfa3bf39d408f3bef200
Elliot Privateer (RIT Student) <ejp4604@ad.rit.edu>
2019-09-25 14:20:07 -0400

    Player Character p1
    Player movement (base)

--- Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
+++ Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 v3_movement;
+    Vector3 v3_jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        v3_movement = Vector3.zero;
+        v3_jumpForce = new Vector3(0f, 3f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        v3_movement = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+            v3_movement.z += .1f;
+        if (Input.GetKey(KeyCode.A))
+            v3_movement.x -= .1f;
+        if (Input.GetKey(KeyCode.S))
+            v3_movement.z -= .1f;
+        if (Input.GetKey(KeyCode.D))
+            v3_movement.x += .1f;
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody>().AddForce(v3_jumpForce, ForceMode.Impulse);
+
+
+        transform.position = v3_movement;
     }
 }
