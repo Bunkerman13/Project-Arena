de488d7214d1c5865af14dfb112e192f7c8bfb1c
ejp4604 <ejp4604@g.rit.edu>
2019-10-25 16:46:35 -0400

    Clean Up
    Cleared out unnecessary code

--- Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
+++ Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 v3_finalMovement;
     Vector3 v3_jumpForce;
     float f_speed;
+    bool b_secondJump;
 
     // Start is called before the first frame update
     void Start()
@@ -15,56 +16,40 @@ public class PlayerMovement : MonoBehaviour
         v3_movement = Vector3.zero;
         v3_jumpForce = new Vector3(0f, 3f, 0f);
         f_speed = 10f;
+        b_secondJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //////// sets current vector for player position
-        //////v3_finalMovement = transform.position;
-
         // Handle base movemenet commands for player
         if (Input.GetKey(KeyCode.W))
         {
             v3_movement = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
-            v3_finalMovement = v3_movement.normalized;// * f_speed;
-            //v3_movement = Camera.main.transform.forward.normalized;// * f_speed;
+            v3_finalMovement = v3_movement.normalized;
         }
         if (Input.GetKey(KeyCode.A))
         {
             v3_movement = new Vector3(-Camera.main.transform.right.x, 0f, -Camera.main.transform.right.z);
             v3_finalMovement = v3_movement.normalized;
-            //v3_movement = -Camera.main.transform.right;// * f_speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
             v3_movement = new Vector3(-Camera.main.transform.forward.x, 0f, -Camera.main.transform.forward.z);
             v3_finalMovement = v3_movement.normalized;
-            //v3_movement = -Camera.main.transform.forward.normalized;// * f_speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
             v3_movement = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
             v3_finalMovement = v3_movement.normalized;
-            //v3_movement = Camera.main.transform.right;// * f_speed;
         }
         if (Input.GetKeyDown(KeyCode.Space))
-        { GetComponent<Rigidbody>().AddForce(v3_jumpForce, ForceMode.Impulse); }
+        {
+            
+            GetComponent<Rigidbody>().AddForce(v3_jumpForce, ForceMode.Impulse);
+        }
+        
 
-        ////////v3_movement = Camera.main.transform.forward.normalized * f_speed;
-
-        //////v3_finalMovement += v3_movement;
-
-        //////// Apply new position to current transformation
-        //////transform.position = v3_finalMovement;
-        ////////Vector3.Lerp(transform.position, v3_finalMovement, Time.time);
-
-
-        ///
-
-        //Vector3 dirVec = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
-        //GetComponent<Rigidbody>().MovePosition(transform.position + v3_movement * Time.deltaTime * f_speed);
-        //v3_movement = Vector3.zero;
         GetComponent<Rigidbody>().MovePosition(transform.position + v3_finalMovement * Time.deltaTime * f_speed);
         v3_finalMovement = Vector3.zero;
 
@@ -73,10 +58,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "Wall")
-        {
-
-            Debug.Log("true");
-        }
+        //GetComponent<Rigidbody>().AddForce(-collision.transform.forward * 1.5f, ForceMode.Impulse);
+       
     }
 }
