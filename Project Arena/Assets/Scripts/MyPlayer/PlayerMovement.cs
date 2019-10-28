19d759e3544ed813d98411d05f20c29dca850a13
Elliot Privateer (RIT Student) <ejp4604@ad.rit.edu>
2019-10-28 15:25:38 -0400

    Update PlayerMovement.cs
    - Added double jump proper
    - Made it so that you can't change direction mid air

--- Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
+++ Project Arena/Assets/Scripts/MyPlayer/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 v3_finalMovement;
     Vector3 v3_jumpForce;
     float f_speed;
-    bool b_secondJump;
+    int i_jumpCount;
 
     // Start is called before the first frame update
     void Start()
@@ -16,42 +16,46 @@ public class PlayerMovement : MonoBehaviour
         v3_movement = Vector3.zero;
         v3_jumpForce = new Vector3(0f, 3f, 0f);
         f_speed = 10f;
-        b_secondJump = false;
+        i_jumpCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Handle base movemenet commands for player
-        if (Input.GetKey(KeyCode.W))
+        if (i_jumpCount < 2)
         {
-            v3_movement = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
-            v3_finalMovement = v3_movement.normalized;
+            // Handle base movemenet commands for player
+            if (Input.GetKey(KeyCode.W))
+            {
+                v3_movement = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
+                v3_finalMovement = v3_movement.normalized;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                v3_movement = new Vector3(-Camera.main.transform.right.x, 0f, -Camera.main.transform.right.z);
+                v3_finalMovement = v3_movement.normalized;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                v3_movement = new Vector3(-Camera.main.transform.forward.x, 0f, -Camera.main.transform.forward.z);
+                v3_finalMovement = v3_movement.normalized;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                v3_movement = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
+                v3_finalMovement = v3_movement.normalized;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Rigidbody>().AddForce(v3_jumpForce, ForceMode.Impulse);
+                i_jumpCount++;
+            }
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            v3_movement = new Vector3(-Camera.main.transform.right.x, 0f, -Camera.main.transform.right.z);
-            v3_finalMovement = v3_movement.normalized;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            v3_movement = new Vector3(-Camera.main.transform.forward.x, 0f, -Camera.main.transform.forward.z);
-            v3_finalMovement = v3_movement.normalized;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            v3_movement = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
-            v3_finalMovement = v3_movement.normalized;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            
-            GetComponent<Rigidbody>().AddForce(v3_jumpForce, ForceMode.Impulse);
-        }
-        
 
         GetComponent<Rigidbody>().MovePosition(transform.position + v3_finalMovement * Time.deltaTime * f_speed);
-        v3_finalMovement = Vector3.zero;
+
+        if(i_jumpCount == 0)
+            v3_finalMovement = Vector3.zero;
 
 
     }
@@ -59,6 +63,7 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //GetComponent<Rigidbody>().AddForce(-collision.transform.forward * 1.5f, ForceMode.Impulse);
-       
+        //if (i_jumpCount == 2)
+            i_jumpCount = 0;
     }
 }
